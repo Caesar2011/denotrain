93f974a6a18a8cd55cca8aecb8534eed57c1fed1
Sebastian Seedorf <mail@sebse.de>
2020-05-21 15:39:50 +0200

    Added Logger to mod.ts

--- mod.ts
+++ mod.ts
@@ -1,12 +1,15 @@
 export { Application, AppOptions } from "./src/Application.ts";
 export { Router, RequestHandler, RequestMethod, Body } from "./src/Router.ts";
 export { Context } from "./src/Context.ts";
+export { Response } from "./src/Response.ts";
+export { Request } from "./src/Request.ts";
+
 export { ViewEngine } from "./src/ViewEngine.ts";
 export {
   CookieValue,
   CookieStorage,
 } from "./src/CookieStorage.ts";
+export { Logger, LogLevel, LogLevelHirachy } from "./src/Logger.ts";
+
 export { ClientError } from "./src/ClientError.ts";
-export { Response } from "./src/Response.ts";
-export { Request } from "./src/Request.ts";
 export { Obj } from "./src/utils/object.ts";
